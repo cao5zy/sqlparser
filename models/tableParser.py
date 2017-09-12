f8e02030ed7fb805ef9126f3a5a0d13a7e432837
zongying.cao <zongying.cao@hpe.com>
2017-09-12 16:28:39 +0000

    add file

---
+++ models/tableParser.py
@@ -0,0 +1,33 @@
+import re
+
+linePatten = '''(?<=from|join)\s+([\w|.|\[|\]]+)'''
+brokenPatten = '''(from|join)\s*$'''
+getBrokenPatten = '''^\s*([\w|.|\[|\]]+)'''
+class TableParser:
+    def __init__(self, twoLinesParser):
+        self.twoLinesParser = twoLinesParser
+
+    def parse(self):
+        result = []
+
+        def parseLine(line):
+            return re.findall(linePatten, line.lower())
+
+        def hasBroken(line):
+            return re.search(brokenPatten, line) != None
+
+        def getBroken(line):
+            return re.search(getBrokenPatten, line).groups()[0]
+        
+        parser = self.twoLinesParser
+        while parser.canRead:
+            parser.read()
+
+            result += parseLine(parser.lineOne)
+
+            if hasBroken(parser.lineOne):
+                result.append(getBroken(parser.lineTwo))
+
+            result += parseLine(parser.lineTwo)
+
+        return result
