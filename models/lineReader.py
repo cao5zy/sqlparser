315ef3ffe848ecbf419d201c4ae4de97941002b4
caozy <cao5zy@163.com>
2017-09-18 15:17:49 +0800

    add new modules

---
+++ models/lineReader.py
@@ -0,0 +1,8 @@
+﻿
+class LineReader:
+	def __init__(self, lines):
+		self.__lines = lines
+
+	def read(self):
+		for line in self.__lines:
+			yield line
---
+++ tests/lineReader_test.py
@@ -0,0 +1,11 @@
+﻿from assertpy import assert_that
+from models.lineReader import LineReader
+def test_lineReader():
+	lines = ["1", "2", "3"]
+
+	lineReader = LineReader(lines)
+
+	for index, line in enumerate(lineReader.read()):
+		index == 0 and assert_that(line).is_equal_to('1')
+		index == 1 and assert_that(line).is_equal_to('2')
+
