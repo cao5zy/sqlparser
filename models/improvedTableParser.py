3a1480a1cc5aedc7447c315905ca2c42b9f2769b
caozy <cao5zy@163.com>
2017-09-18 16:09:23 +0800

    add new modules

---
+++ models/improvedTableParser.py
@@ -0,0 +1,30 @@
+﻿# improve table parser
+import re
+
+linePatten = '''(?<=from|join)\s+([\w|.|\[|\]]+)'''
+brokenPatten = '''(from|join)\s*$'''
+class ImprovedTableParser:
+    def __init__(self, lineParser):
+        self.lineParser = lineParser
+
+    def parse(self):
+        result = []
+
+        def getLine(line):
+        	return line if not hasBroken(line) \
+        		else " ".join([line, getLine(self.lineParser.read())]) \
+        		if self.lineParser.canRead \
+        		else line
+
+
+        def parseLine(line):
+            return re.findall(linePatten, line.lower())
+
+        def hasBroken(line):
+            return re.search(brokenPatten, line) != None
+
+        while self.lineParser.canRead:
+            result += parseLine(getLine(self.lineParser.read()))
+
+        return list(set(result))
+
---
+++ tests/improvedTableParser_test.py
@@ -0,0 +1,26 @@
+﻿# test improvedTableParser
+from assertpy import assert_that
+from models.lineReader import LineReader
+from models.improvedTableParser import ImprovedTableParser
+
+def test_improvedTableParser_normal():
+	lines = ["select * from abc"]
+
+	tableParser = ImprovedTableParser(LineReader(lines))
+
+	results = tableParser.parse()
+	assert_that(results).contains('abc')
+
+def test_improvedTableParser_multiple():
+	lines = ["select * from abc", 
+	"select * from acd",
+	"select * from",
+	"abd"]
+
+	tableParser = ImprovedTableParser(LineReader(lines))
+
+	results = tableParser.parse()
+	assert_that(results).contains('abc')
+	assert_that(results).contains('acd')
+	assert_that(results).contains('abd')
+
